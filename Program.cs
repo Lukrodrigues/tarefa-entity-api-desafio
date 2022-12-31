27c7627d42473a54de0520a12e80ac0d823f004f
Luciano Rodrigues <lukrodrigues68@hotmail.comclear$ git config --global user.email lukrodrigues68@hotmail.comexitcdexit>
2022-12-31 19:36:46 -0300

    Implementado a logica nas tarefas no controller

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using TrilhaApiDesafio.Context;
 
 var builder = WebApplication.CreateBuilder(args);
